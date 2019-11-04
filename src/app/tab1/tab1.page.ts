475e5fd805fc1cf82ce27a1de045ab36ad2559bf
marcio2106 <marciomg2106@gmail.com>
2019-11-04 13:07:44 -0200

    atu

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { PostProvider } from '../providers/post-provider';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/Storage';
 
@@ -10,10 +11,15 @@ import { Storage } from '@ionic/Storage';
 })
 export class Tab1Page implements OnInit {
 
+  anggota: any;
   username: string;
   
+  customers: any = [];
+  limit: number = 13; // LIMIT GET PERDATA
+  start: number = 0;
   constructor(
   	private router: Router,
+  	private postPvdr: PostProvider,
     private storage: Storage,
     public toastCtrl: ToastController
   ) { }
@@ -21,11 +27,81 @@ export class Tab1Page implements OnInit {
   ngOnInit() {
   }
 
+  ionViewWillEnter(){
+    this.storage.get('session_storage').then((res)=>{
+      this.anggota = res;
+      this.username = this.anggota.username;
+      console.log(res);
+    });
+
+  	this.customers = [];
+  	this.start = 0;
+  	this.loadCustomer();
+  }
+
+  addCustomer(){
+  	this.router.navigate(['/addcustomer']);
+  }
+
+  updateCustomer(id,name,desc){
+  	this.router.navigate(['/addcustomer/' + id + '/' + name + '/' + desc]);
+  }
+
+  showCustomer(id,name,desc){
+  	this.router.navigate(['/showcustomer/' + id + '/' + name + '/' + desc]);
+  }
+
+  doRefresh(event){
+  	setTimeout(() =>{
+  		this.ionViewWillEnter();
+  		event.target.complete();
+  	}, 500);
+  }
+
+  loadData(event:any){
+  	this.start += this.limit;
+  	setTimeout(() =>{
+  	this.loadCustomer().then(()=>{
+  		event.target.complete();
+  	});
+  	}, 500);
+  }
+
+  delCustomer(id){
+
+  	let body = {
+  			aksi : 'delete',
+  			customer_id : id
+  		};
+
+  		this.postPvdr.postData(body, 'proses-api.php').subscribe(data => {
+  			this.ionViewWillEnter();
+  		});
+
+  }
+
+  loadCustomer(){
+  	return new Promise(resolve => {
+  		let body = {
+  			aksi : 'getdata',
+  			limit : this.limit,
+  			start : this.start,
+  		};
+
+  		this.postPvdr.postData(body, 'proses-api.php').subscribe(data => {
+  			for(let customer of data.result){
+  				this.customers.push(customer);
+  			}
+  			resolve(true);
+  		});
+  	});
+  }
+
   async prosesLogout(){
     this.storage.clear();
     this.router.navigate(['/login']);
     const toast = await this.toastCtrl.create({
-        message: 'Logout com Sucesso',
+        message: 'logout succesful',
         duration: 3000
       });
     toast.present();
